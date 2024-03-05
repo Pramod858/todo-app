2539b0986050123ebc1c6b86523d19eaa22a22af
Pramod Badiger <80105491+Pramod858@users.noreply.github.com>
2024-03-05 23:22:03 +0530

    updated

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,8 +4,5 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull pramod858/todo-app:latest
 
-# Remove the existing container (if any)
-docker rm todo-app || true
-
 # Run the Docker image as a container
 docker run -d -p 8080:8080 pramod858/todo-app:latest
--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
-# Stop the Docker container
-docker stop todo-app || true
+# Stop and remove existing Docker container (if any) using port 8080
+existing_container=$(docker ps -q -f "ancestor=pramod858/todo-app" -f "expose=8080")
+
+if [ -n "$existing_container" ]; then
+    echo "Stopping and removing existing Docker container..."
+    docker stop "$existing_container"
+    docker rm "$existing_container"
+fi
+
 
